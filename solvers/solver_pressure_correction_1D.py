a2d8071ff337661641268fbd9e469cadab9e6755
MKAR2022 <tapukor.moinak11@gmail.com>
2026-05-11 18:29:00 +0200

    comit

--- solvers/solver_pressure_correction_1D.py
+++ solvers/solver_pressure_correction_1D.py
@@ -91,12 +91,12 @@ def v_cor(w, r1, r2, r3, r4, R, L, d, gm, dx):
 
 
 tf = 2.0
-kappa = 0.7
+kappa = 0.001
 nu = 1.0
 gamma = 2.0
 rho_initial_condition = fv.initial_condition.gaussian_rho
 u_initial_condition = fv.initial_condition.constant_u
-case = fv.computational_case(a = -2.0, b = 2.0, Tf = 0.5, N = 100, dt = 0.001, ng = 1)
+case = fv.computational_case(a = -2.0, b = 2.0, Tf = 0.5, N = 1000, dt = 0.001, ng = 1)
 "-------initialization of the scheme--------------"
 a = case.a
 b = case.b
@@ -176,7 +176,7 @@ E_0 = E_0 * cell_size
 #------------------------
 """Time-looping begins"""
 #------------------------
-num_steps = 1000
+num_steps = 200
 energy = np.zeros(num_steps)
 tm = np.zeros(num_steps)
 for n in range(num_steps):
@@ -227,9 +227,9 @@ for n in range(num_steps):
     flx = f_ev - kappa * nu * f_dv
 
     """Matrix blocks corresponding to the linear system for solving tilde{w} and v"""
-    W1 = d_linsolv(flx, rho_0, c1, c2) #tilde{w} part of tilde{w} eqn
+    W1 = d_linsolv(flx, rho_0 * (1+math.pow(cell_size, 0.25)), c1, c2) #tilde{w} part of tilde{w} eqn
     V1 = d_linsolv_dif(rho_0, d) #v part of tilde{w} eqn
-    V2 = d_linsolv(flx, rho_0, c1, c3) #v part of v eqn
+    V2 = d_linsolv(flx, rho_0 * (1+math.pow(cell_size, 0.25)), c1, c3) #v part of v eqn
     W2 = d_linsolv_dif(rho_0, lda2) #tilde{w} part of w eqn
 
     M = build_mtx(W1,V1, W2, V2)
@@ -333,11 +333,11 @@ tv[0] = (rho_init[0] - rho_init[-1])/(cell_size * 0.5 * (rho_init[0] + rho_init[
 tv[-1] = (rho_init[0] - rho_init[-1])/(cell_size * 0.5 * (rho_init[0] + rho_init[-1]))               # right wrap to close periodicity
 #ax.plot(x_prim, rho_0, label=r"$\rho$, T_final")
 #ax.plot(x_dual, w_0, label=r"$w$, T_final")
-#ax.plot(x_dual, v_init, label=r"$v$, T_final")  
+ax.plot(x_dual, v_init, label=r"$v$, T_final")  
 u = w_0 + kappa * nu * v_init
 #ax.plot(x_dual, u, label=r"$u$, T_final")
 #ax.plot(x_dual, v_init, label=r"$v$, T_final")
-#ax.plot(x_dual, tv, label=r"$\tilde{v}$, T_final")
+ax.plot(x_dual, tv, label=r"$\tilde{v}$, T_final")
 ax.legend()
 L1_tot_final = np.sum(rho_0)
 error_tot = L1_tot - L1_tot_final
@@ -347,9 +347,9 @@ error_v = v_init - tv
 norm_error_v = math.sqrt(cell_size) * np.abs(error_v)#requires fixing
 print("error_v:", norm_error_v)
 T_f = num_steps * dt
-ax.plot(tm, energy, label=r"Total $\kappa$-entropy")
-ax.set_xlabel(r"$t$")
-ax.set_ylabel(r"Total $\kappa$-entropy")
+# ax.plot(tm, energy, label=r"Total $\kappa$-entropy")
+# ax.set_xlabel(r"$t$")
+# ax.set_ylabel(r"Total $\kappa$-entropy")
 ax.legend()
 #plt.plot(energy)
 #plt.xlabel("time steps")
